9815439e3bf7656084f0d7130f8b02f3b971bf43
jcamiloguz <jcamiloguz1@gmail.com>
2021-10-20 20:56:10 +0000

    adding swagger docs

--- apirest.py
+++ apirest.py
@@ -1,9 +1,24 @@
 #!flask/bin/python
-from flask import Flask, jsonify
 from flask import abort
 from flask import request
 
-app = Flask(__name__)
+from flask import render_template
+
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec_webframeworks.flask import FlaskPlugin
+from flask import Flask,jsonify,send_from_directory
+from marshmallow import Schema, fields
+from datetime import date
+
+app = Flask(__name__, instance_relative_config=True)
+
+spec = APISpec( 
+    title='Flask-api-swagger-doc',
+    version='1.0.0.',
+    openapi_version='3.0.2',
+    plugins=[FlaskPlugin(),MarshmallowPlugin()]
+)
 
 books = [
     {
@@ -19,26 +34,74 @@ books = [
         'author': 'Gabo'
     }
 ]
+@app.route('/api/swagger.json')
+def create_swagger_spec():
+        return jsonify(spec.to_dict())
+
+
+class BookResponseSchema(Schema):
+        id = fields.Int()
+        title = fields.Str()
+        description = fields.Str()
+        author = fields.Str()
+class DeleteResponseSchema(Schema):
+        result = fields.Bool()
+
+class BookListResponseSchema(Schema):
+        book_list = fields.List(fields.Nested(BookResponseSchema))
 
 # Get all books
 # For testing: curl -i http://localhost:5000/books 
 @app.route('/books', methods=['GET'])
 def get_books():
-    return jsonify({'books': books})
+    """Get List of Books
+        ---
+        get:
+            description: Get List of Books
+            responses:
+                200:
+                    description: Return an Books list
+                    content:
+                        application/json:
+                            schema: BookListResponseSchema
+    """
+    return BookListResponseSchema().dump({'book_list': books})
 
 # Get one book by id
 # For testing: curl -i http://localhost:5000/books/2 
 @app.route('/books/<int:book_id>', methods=['GET'])
 def get_book(book_id):
+    """Get a Book
+        ---
+        get:
+            description: Get a Book
+            responses:
+                200:
+                    description: Return a Book 
+                    content:
+                        application/json:
+                            schema: BookListResponseSchema
+    """
     book = [book for book in books if book['id'] == book_id]
     if len(book) == 0:
         abort(404)
-    return jsonify({'book': book[0]})
+    return BookListResponseSchema().dump({'book_list': book[0]})
 
 # Add new book
 # For testing: curl -i -H "Content-Type: application/json" -X POST -d '{"title":"El libro"}' http://localhost:5000/books
 @app.route('/books', methods=['POST'])
 def create_book():
+    """Create a new Book
+        ---
+        post:
+            description: Create a Book
+            responses:
+                200:
+                    description: Create a Book 
+                    content:
+                        application/json:
+                            schema: BookListResponseSchema
+    """
     if not request.json or not 'title' in request.json:
         abort(400)
     book = {
@@ -48,29 +111,68 @@ def create_book():
         'author': request.json.get('author', ""),
     }
     books.append(book)
-    return jsonify({'book': book}), 201
+    return BookListResponseSchema().dump({'book_list': book}), 201
 
 # Edit a Book
 # For testing: curl -i -H "Content-Type: application/json" -X PUT -d '{"author":"Jorgito"}' http://localhost:5000/books/2
 @app.route('/books/<int:book_id>', methods=['PUT'])
 def update_book(book_id):
+    """Update a specific Book
+        ---
+        put:
+            description: Update a Book
+            responses:
+                200:
+                    description: Update a Book 
+                    content:
+                        application/json:
+                            schema: BookListResponseSchema
+    """
     book = [book for book in books if book['id'] == book_id]
     if len(book) == 0 or not request.json:
         abort(404)
     book[0]['title'] = request.json.get('title', book[0]['title'])
     book[0]['description'] = request.json.get('description', book[0]['description'])
     book[0]['author'] = request.json.get('author', book[0]['author'])
-    return jsonify({'book': book[0]})
+    return BookListResponseSchema().dump({'book_list': book[0]})
 
 # Delete a Book
 # For testing: curl -i -H "Content-Type: application/json" -X DELETE http://localhost:5000/books/1
 @app.route('/books/<int:book_id>', methods=['DELETE'])
 def delete_book(book_id):
+    """Delete a specific Book
+        ---
+        delete:
+            description: Delete a Book
+            responses:
+                200:
+                    description: Delete a Book 
+                    content:
+                        application/json:
+                            schema: DeleteResponseSchema
+    """
     book = [book for book in books if book['id'] == book_id]
     if len(book) == 0:
         abort(404)
     books.remove(book[0])
-    return jsonify({'result': True})
+    return  DeleteResponseSchema({'result': True})
+
+
+with app.test_request_context():
+    spec.path(view=get_books)
+    spec.path(view=get_book)
+    spec.path(view=update_book)
+    spec.path(view=create_book)
+    spec.path(view=delete_book)
+
+
+@app.route('/docs')
+@app.route('/docs/<path:path>')
+def swagger_docs(path=None):
+    if not path or path == 'index.html':
+        return render_template('index.html',base_url='/docs')
+    else:
+        return send_from_directory('static',path)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
